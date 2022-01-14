3a63becc2853c6eb0f52e0df852b5b46acf6c4c0
Pirnes <lassepirnes@gmail.com>
2022-01-14 09:14:41 +0200

    JatkanShakki (Tic Tac Toe)

---
+++ JatkanShakki/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace JatkanShakki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelPeliAlue = new System.Windows.Forms.Panel();
+            this.TextBoxKumpiPelaaVuoro = new System.Windows.Forms.TextBox();
+            this.TextBoxKumpiPelaa = new System.Windows.Forms.TextBox();
+            this.NappiPelaaja = new System.Windows.Forms.Button();
+            this.LabelXWINS = new System.Windows.Forms.Label();
+            this.LabelOWINS = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PanelPeliAlue
+            // 
+            this.PanelPeliAlue.Location = new System.Drawing.Point(12, 78);
+            this.PanelPeliAlue.Name = "PanelPeliAlue";
+            this.PanelPeliAlue.Size = new System.Drawing.Size(500, 500);
+            this.PanelPeliAlue.TabIndex = 0;
+            // 
+            // TextBoxKumpiPelaaVuoro
+            // 
+            this.TextBoxKumpiPelaaVuoro.BackColor = System.Drawing.Color.Black;
+            this.TextBoxKumpiPelaaVuoro.Font = new System.Drawing.Font("Venus Rising", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxKumpiPelaaVuoro.ForeColor = System.Drawing.Color.Yellow;
+            this.TextBoxKumpiPelaaVuoro.Location = new System.Drawing.Point(284, 39);
+            this.TextBoxKumpiPelaaVuoro.Multiline = true;
+            this.TextBoxKumpiPelaaVuoro.Name = "TextBoxKumpiPelaaVuoro";
+            this.TextBoxKumpiPelaaVuoro.ReadOnly = true;
+            this.TextBoxKumpiPelaaVuoro.Size = new System.Drawing.Size(136, 33);
+            this.TextBoxKumpiPelaaVuoro.TabIndex = 0;
+            this.TextBoxKumpiPelaaVuoro.Text = "Turn";
+            this.TextBoxKumpiPelaaVuoro.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TextBoxKumpiPelaa
+            // 
+            this.TextBoxKumpiPelaa.BackColor = System.Drawing.Color.Black;
+            this.TextBoxKumpiPelaa.Font = new System.Drawing.Font("Venus Rising", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxKumpiPelaa.ForeColor = System.Drawing.Color.Yellow;
+            this.TextBoxKumpiPelaa.Location = new System.Drawing.Point(74, 39);
+            this.TextBoxKumpiPelaa.Multiline = true;
+            this.TextBoxKumpiPelaa.Name = "TextBoxKumpiPelaa";
+            this.TextBoxKumpiPelaa.ReadOnly = true;
+            this.TextBoxKumpiPelaa.Size = new System.Drawing.Size(152, 33);
+            this.TextBoxKumpiPelaa.TabIndex = 0;
+            this.TextBoxKumpiPelaa.Text = "player";
+            this.TextBoxKumpiPelaa.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // NappiPelaaja
+            // 
+            this.NappiPelaaja.BackColor = System.Drawing.Color.White;
+            this.NappiPelaaja.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.NappiPelaaja.Location = new System.Drawing.Point(232, 39);
+            this.NappiPelaaja.Name = "NappiPelaaja";
+            this.NappiPelaaja.Size = new System.Drawing.Size(46, 33);
+            this.NappiPelaaja.TabIndex = 1;
+            this.NappiPelaaja.UseVisualStyleBackColor = false;
+            // 
+            // LabelXWINS
+            // 
+            this.LabelXWINS.AutoSize = true;
+            this.LabelXWINS.Font = new System.Drawing.Font("Venus Rising", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelXWINS.Location = new System.Drawing.Point(12, 13);
+            this.LabelXWINS.Name = "LabelXWINS";
+            this.LabelXWINS.Size = new System.Drawing.Size(0, 22);
+            this.LabelXWINS.TabIndex = 2;
+            // 
+            // LabelOWINS
+            // 
+            this.LabelOWINS.AutoSize = true;
+            this.LabelOWINS.Font = new System.Drawing.Font("Venus Rising", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelOWINS.Location = new System.Drawing.Point(280, 13);
+            this.LabelOWINS.Name = "LabelOWINS";
+            this.LabelOWINS.Size = new System.Drawing.Size(0, 22);
+            this.LabelOWINS.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(523, 586);
+            this.Controls.Add(this.LabelOWINS);
+            this.Controls.Add(this.LabelXWINS);
+            this.Controls.Add(this.NappiPelaaja);
+            this.Controls.Add(this.PanelPeliAlue);
+            this.Controls.Add(this.TextBoxKumpiPelaaVuoro);
+            this.Controls.Add(this.TextBoxKumpiPelaa);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JätkänShakki";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelPeliAlue;
+        private System.Windows.Forms.TextBox TextBoxKumpiPelaa;
+        private System.Windows.Forms.Button NappiPelaaja;
+        private System.Windows.Forms.TextBox TextBoxKumpiPelaaVuoro;
+        private System.Windows.Forms.Label LabelXWINS;
+        private System.Windows.Forms.Label LabelOWINS;
+    }
+}
+
---
+++ JatkanShakki/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace JatkanShakki
+{
+    public partial class Form1 : Form
+    {
+        readonly Button[,] napit = new Button[20,20];
+        List<Button> Osumat = new List<Button>();
+        readonly Bitmap X = new Bitmap(Properties.Resources.X);
+        readonly Bitmap O = new Bitmap(Properties.Resources.O);
+        int XVoitot = 0, OVoitot = 0;
+
+        public Form1()
+        {
+                InitializeComponent();
+                LuodaanNapit();
+                X.MakeTransparent();
+                O.MakeTransparent();
+
+                LabelXWINS.Text = "Player X wins = " + XVoitot;
+                LabelOWINS.Text = "Player O wins = " + OVoitot;
+
+                if (NappiPelaaja.Image == null)
+                {
+                    NappiPelaaja.Image = X;
+                }
+        }
+
+        //luodaan napit pelialueelle
+        private void LuodaanNapit()
+        {
+            for (int i=0; i<20; i++)
+            {
+                for (int j=0; j<20; j++)
+                {
+                    napit[i, j] = new Button();
+                    napit[i, j].Size = new Size(25, 25);
+                    napit[i, j].Location = new Point(i * 25, j * 25);
+                    napit[i, j].FlatStyle = FlatStyle.Flat;
+                    napit[i, j].Click += new EventHandler(Napin_Painallus);
+                    PanelPeliAlue.Controls.Add(napit[i, j]);
+                }
+            }
+        }
+
+        //tarkistaa kumpi pelaaja vuorossa ja tekee vuoron vaihdon
+        private void VaihdaPelaajaa()
+        {
+            OnkoViisiTaynna();
+
+            if (NappiPelaaja.Image == X)
+            {
+                NappiPelaaja.Image = O;
+            }
+
+            else
+            {
+                NappiPelaaja.Image = X;
+            }
+        }
+
+        private void Napin_Painallus(object sender, EventArgs e)
+        {
+            Button nappi = sender as Button;
+            
+            if (nappi.Image != null || nappi.Text != "")
+            {
+                return;
+            }
+
+            //asetetaan vuorossa olevan pelaajan kuvake myös pelilaudan napin kuvakkeeksi
+            nappi.Image = NappiPelaaja.Image;
+            VaihdaPelaajaa();
+        }
+
+        //tarkistaa pelaajien osumien määrän
+        private void OnkoViisiTaynna()
+        {
+            //pystysuoran testi
+            for (int i = 0; i < 20; i++)
+            {
+                Osumat = new List<Button>();
+                for (int j = 0; j < 20; j++)
+                {
+                    EtsitaanOsumat(napit[i, j]);
+                }
+            }
+
+            //vaakasuoran testi
+            for (int i = 0; i < 20; i++)
+            {
+                Osumat = new List<Button>();
+                for (int j = 0; j < 20; j++)
+                {
+                    EtsitaanOsumat(napit[j, i]);
+                }
+            }
+
+            //diagonaali testi vasen yläkulma --> oikea alakulma
+            for (int k = 0; k < 20; k++)
+            {
+                Osumat = new List<Button>();
+                for (int i = k, j =0; i < 20; i++,j++)
+                {
+                    EtsitaanOsumat(napit[i, j]);
+                }
+
+                for (int i = 0, j = k; j < 20; i++,j++)
+                {
+                    EtsitaanOsumat(napit[i, j]);
+                }
+            }
+
+            //diagonaali testi vasen alakulma --> oikea yläkulma
+            for (int k = 0; k < 20; k++)
+            {
+                Osumat = new List<Button>();
+                for (int i = 19, j = k; j < 20; i--, j++)
+                {
+                    EtsitaanOsumat(napit[i, j]);
+                }
+                Osumat = new List<Button>();
+                for (int i = k, j = 0; i >= 0; i--, j++)
+                {
+                    EtsitaanOsumat(napit[i, j]);
+                }
+            }
+        }
+
+        //tarkistaa löytyykö pelaajilta 5 perättäistä osumaa
+        private void EtsitaanOsumat(Button nappi)
+        {
+            if (nappi.Image != NappiPelaaja.Image)
+            {
+                Osumat.Clear();
+            }
+
+            else
+            {
+                Osumat.Add(nappi);
+            }
+
+            if (Osumat.Count == 5)
+            {
+                NaytaVoittajaNapit();
+                return;
+            }
+        }
+
+        //näyttää syntyneen suoran sekä alustaa seuraavan pelin ja tarkistaa onko kummallakaan pelaajalla 5 voittoa
+        //Tulostaa myös messageboxin jossa kysytään aloitetaanko peli alusta vai lopetetaanko
+        private void NaytaVoittajaNapit()
+        {
+            if (NappiPelaaja.Image == X)
+            {
+                XVoitot++;
+                LabelXWINS.Text = "Player X wins = " + XVoitot;
+            }
+
+            if (NappiPelaaja.Image == O)
+            {
+                OVoitot++;
+                LabelOWINS.Text = "Player O wins = " + OVoitot;
+            }
+            
+            foreach (var a in Osumat)
+            {
+                a.BackColor = Color.Green;
+                Thread.Sleep(100);
+                Refresh();
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                TextBoxKumpiPelaa.BackColor = Color.Green;
+                TextBoxKumpiPelaaVuoro.Text = "WINS!!";
+                TextBoxKumpiPelaaVuoro.BackColor = Color.Green;
+                Refresh();
+                Thread.Sleep(500);
+                TextBoxKumpiPelaa.BackColor = Color.Black;
+                TextBoxKumpiPelaaVuoro.Text = "WINS!!";
+                TextBoxKumpiPelaaVuoro.BackColor = Color.Black;
+                Refresh();
+                Thread.Sleep(500);
+            }
+            TextBoxKumpiPelaaVuoro.Text = "turn";
+            Osumat.Clear();
+            PanelPeliAlue.Controls.Clear();
+            LuodaanNapit();
+           
+            if (XVoitot == 5 || OVoitot == 5)
+            {
+                if (MessageBox.Show("YES to restart, or NO to quit.", "Game over", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Application.Restart();
+                }
+
+                if (MessageBox.Show("YES to restart, or NO to quit.", "Game over", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
