b28353c37a90f38507883662e77f552ae6f2ef11
Pirnes <lassepirnes@gmail.com>
2022-02-07 16:42:32 +0200

    Random toiminne lisatty

---
+++ Chuck_Norris_Jokes_Api/Shared/RandomResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Chuck_Norris_Jokes_Api.Shared
+{
+    public class RandomResponse
+    {
+        public string Type { get; set; }
+        public SingleJoke Value { get; set; }
+    }
+
+    public class SingleJoke
+    {
+        public int Id { get; set; }
+        public string Joke { get; set; }
+        public string[] Categories { get; set; }
+    }
+}
