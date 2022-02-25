15f6058650afb6ede486ec07b8a495332e21f873
Pirnes <lassepirnes@gmail.com>
2022-02-25 11:36:12 +0200

    FunPage projekti lisatty

---
+++ FunPage/Shared/CNJokes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunPage.Shared
+{
+        public class RandomJoke
+        {
+            public string Type { get; set; }
+            public SingleJoke Value { get; set; }
+        }
+
+        public class SingleJoke
+        {
+            public int Id { get; set; }
+            public string Joke { get; set; }
+            public string[] Categories { get; set; }
+        }
+    
+}
---
+++ FunPage/Shared/OtherJokes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunPage.Shared
+{
+    public class RandomDadJoke
+    {
+        public string Type { get; set; }
+        public SingleDadJoke Value { get; set; }
+    }
+
+    public class SingleDadJoke
+    {
+        public int Id { get; set; }
+        public string Joke { get; set; }
+        public string[] Categories { get; set; }
+    }
+}
