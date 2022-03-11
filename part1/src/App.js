43e6ab9b77cb0c00b34cbae460a4372b93e06280
Pirnes Lasse <Lasse.Pirnes@ramirent.fi>
2022-03-11 09:37:35 +0200

    Fullstack kurssi osa 0 valmis

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,31 @@
-const App = () => (
-  <div>
-    <p>Hello Lasse!!</p>
-  </div>
-)
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old.</p>
+    </div>
+  )
+}
 
+const Testi = (props) => {
+  return (
+    <div>
+      <p>{props.name2} and {props.name} are together {props.age} years!</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const nimi = 'pekka'
+  const nimi2 = 'Maya'
+  const ika = 10
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10}/>
+      <Hello name={nimi} age={ika}/>
+      <Testi name2={nimi2} name={nimi} age={26 + 10 + ika}/>
+
+    </div>
+  )
+}
 export default App
