570a34598c8be8bb9cbd957a53930d84b2b3c54e
Pirnes Lasse <Lasse.Pirnes@ramirent.fi>
2022-03-16 12:16:43 +0200

    Osa 1c valmis

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,31 +1,72 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old.</p>
-    </div>
-  )
-}
+//const Hello = ({name, age}) => {
+//  const bornYear = () => new Date().getFullYear() - age
+//
+//  return (
+//    <div>
+//      <p>Hello {name}, you are {age} years old.</p>
+//      <p>So you were probably born {bornYear()}?</p>
+//    </div>
+//  )
+//}
 
-const Testi = (props) => {
-  return (
-    <div>
-      <p>{props.name2} and {props.name} are together {props.age} years!</p>
-    </div>
-  )
-}
+// const App = (props) => {
+//   const nimi = 'pekka'
+//   const ika = 10
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10}/>
+//       <Hello name={nimi} age={ika}/>
+//     </div>
+//   )
+// }
+import { useState } from "react"
+
+const Display = ({counter}) => <div>{counter}</div>
+  
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
-  const nimi = 'pekka'
-  const nimi2 = 'Maya'
-  const ika = 10
+  const [ counter, setCounter] = useState(0)
+  
+  const increaseByOne = () => setCounter(counter +1)
+  const decreaseByOne = () => setCounter(counter -1)  
+  const setToZero = () => setCounter(0)
+    
+
+  // const handleClick = () => {
+  //   console.log('clicked')
+  // }
+ 
+  // setTimeout(
+  //   () => setCounter(counter +1),
+  //   1000
+  // )
+ 
+  // console.log('rendering...', counter)
+ 
+  console.log('clicked', counter)
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10}/>
-      <Hello name={nimi} age={ika}/>
-      <Testi name2={nimi2} name={nimi} age={26 + 10 + ika}/>
-
+    <Display counter={counter}/>  
+    <Button 
+      handleClick={increaseByOne}
+      text = 'plus'
+    />
+    <Button 
+      handleClick={setToZero}
+      text = 'zero'
+    />
+    <Button 
+      handleClick={decreaseByOne}
+      text = 'minus'
+    />
     </div>
   )
 }
+
 export default App
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,18 @@
 
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.render(<App counter={counter} />,
+  document.getElementById('root'))
+};
+
+setInterval(() => {
+refresh()
+counter +=2
+}, 1000)
+
 
 
