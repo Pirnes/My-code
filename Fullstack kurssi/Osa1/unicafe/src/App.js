1b7764da7aa68eafe03a6f115982c79095eff291
Pirnes Lasse <Lasse.Pirnes@ramirent.fi>
2022-03-17 09:01:15 +0200

    unicafe step1

---
+++ Fullstack kurssi/Osa1/unicafe/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+const Display = props => <div>{props.text}{props.value}</div>
+
+const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>give feedback</h2><br></br>
+      <Button handleClick={() => setGood(good + 1)} text='good'/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'/>
+      <Button handleClick={() => setBad(bad + 1)} text='bad'/>
+      <br></br>
+      <h2>statistics</h2>
+      <Display value={good} text='good: '/>
+      <Display value={neutral} text='neutral: '/>
+      <Display value={bad} text='bad: '/>
+    </div>
+  )
+}
+
+export default App;
