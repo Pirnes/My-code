6e0ddb41acb08a26295af0976d18a2e694480d1b
Pirnes Lasse <Lasse.Pirnes@ramirent.fi>
2022-03-17 10:58:13 +0200

    unicafe step2

--- Fullstack kurssi/Osa1/unicafe/src/App.js
+++ Fullstack kurssi/Osa1/unicafe/src/App.js
@@ -11,18 +11,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good+neutral+bad
+  const average = (good - bad)/all
+  const positive = good/all*100+' %'
 
   return (
     <div>
-      <h2>give feedback</h2><br></br>
+      <h2>give feedback:</h2>
+      <p></p>
       <Button handleClick={() => setGood(good + 1)} text='good'/>
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'/>
       <Button handleClick={() => setBad(bad + 1)} text='bad'/>
-      <br></br>
-      <h2>statistics</h2>
+      <p></p>
+      <h2>statistics:</h2>
       <Display value={good} text='good: '/>
       <Display value={neutral} text='neutral: '/>
       <Display value={bad} text='bad: '/>
+      <p></p>
+      <Display value={all} text='all: '/>
+      <Display value={average} text='average: '/>
+      <Display value={positive} text='positive: '/>
     </div>
   )
 }
