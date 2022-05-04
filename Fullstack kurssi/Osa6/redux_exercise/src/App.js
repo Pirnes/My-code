46f73dabc160919105bd054aaa1f8c9dac39d24b
Pirnes Lasse <Lasse.Pirnes@ramirent.fi>
2022-05-04 14:23:52 +0300

    Osa6 redux_exercise

---
+++ Fullstack kurssi/Osa6/redux_exercise/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { legacy_createStore } from 'redux'
+import  noteReducer  from './reducers/noteReducer'
+
+const store = legacy_createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+const App = () => {
+  return(
+    <div>
+      <ul>
+        {store.getState().map(note=>
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+        </ul>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
+
+export default App;
