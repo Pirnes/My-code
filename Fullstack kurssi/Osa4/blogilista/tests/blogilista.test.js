bb4c794b771a8b7c6852005793ce0bda64e29e17
Pirnes <lassepirnes@gmail.com>
2022-11-08 10:28:23 +0200

    up to date 11/2022

--- Fullstack kurssi/Osa4/blogilista/tests/blogilista.test.js
+++ Fullstack kurssi/Osa4/blogilista/tests/blogilista.test.js
@@ -1,47 +1,44 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
-const helper = require('./test_helper')
-const api = supertest(app)
-const Blog = require('../models/blog')
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const helper = require("./test_helper");
+const api = supertest(app);
+const Blog = require("../models/blog");
 
-
-
-test('Blogs are returned as json', async () => {
+test("Blogs are returned as json", async () => {
   await api
-  .get('/api/blogs')
-  .expect(201)
-  .expect('Content-Type', /application\/json/)
-})
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+test("all blogs are returned", async () => {
+  const response = await api.get("/api/blogs");
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
 
-test('is id id', async () => {
-  expect(helper.initialBlogs[0].id).toBeDefined()
-})
+test("is id id", async () => {
+  expect(helper.initialBlogs[0].id).toBeDefined();
+});
 
-test('does POST works', async () => {
+test("does POST works", async () => {
   const newBlog = {
-    "author": "Lasse Pirnes",
-    "likes": 0, 
-    "title": "testi blogi",
-    "url": "https://www.google.fi"
-  }
+    author: "Lasse Pirnes",
+    title: "testi blogi",
+    url: "https://www.google.fi",
+  };
 
-  // await api 
-  // .post('/api/blogs')
-  // .send(newBlog)
-  // .expect(201)
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
 
-  const response = await Blog.find({})
-  console.log(response)
-  expect(helper.initialBlogs.length).toHaveLength(2)
-})
-
+  const response = await Blog.find({});
+  console.log(response);
+  expect(helper.initialBlogs.length).toBe(response.length - 1);
+});
 
 afterAll(() => {
-  mongoose.connection.close()
-})
+  mongoose.connection.close();
+});
